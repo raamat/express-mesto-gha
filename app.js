cfd321dd71110df1a62cfc7e1b606d6169f3656b
Raamat <raamat@bk.ru>
2023-07-23 22:39:31 +0300

    Реализовать временное решение авторизации

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
-const routes = require("./routes/users");
+const routes = require('./routes/users');
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64bce3b56d5d6d4aa8429b83',
+  };
+
+  next();
+});
+
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {
+  await mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
     useNewUrlParser: true,
     useUnifiedTopology: false,
   });
