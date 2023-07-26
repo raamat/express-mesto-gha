69b406cd7aa4fd138f6c9864d9354b663867b33b
Raamat <raamat@bk.ru>
2023-07-26 20:55:12 +0300

    Добавить обработку ошибок 400 и 404 пользователю

--- controllers/users.js
+++ controllers/users.js
@@ -1,25 +1,42 @@
+const mongoose = require('mongoose');
 const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send(users))
+    .then((users) => res.status(200).send(users))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.getUserById = (req, res) => {
   const { id } = req.params;
 
-  User.findById(id)
-    .then((user) => res.send(user))
-    .catch(() => {
-      res.send('Такого пользователя не существует');
+  return User.findById(id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'Нет пользователя с таким id' });
+      }
+
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err instanceof mongoose.CastError) {
+        return res.status(400).send({ message: ' Ошибка в введеных данных' });
+      }
+      res.status(500).send({ message: `Произошла ошибка в работе сервера ${err}` });
     });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+  return User.create({ name, about, avatar })
+    .then((user) => {
+      res.status(201).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Ошибка в введенных данных' });
+      }
+      res.status(500).send({ message: `Произошла ошибка в работе сервера ${err}` });
+    });
 };
