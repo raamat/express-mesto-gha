479d8569ffe423d54c7397b4a0d2144b3d3ac8c5
Raamat <raamat@bk.ru>
2023-07-28 08:20:42 +0300

    Создать роуты PUT и DELETE карточек

--- controllers/cards.js
+++ controllers/cards.js
@@ -22,10 +22,10 @@ module.exports.createCard = (req, res) => {
 module.exports.cardDelete = (req, res, next) => {
   const { id } = req.params;
 
-  Card.findById(id)
+  return Card.findById(id)
     .then((card) => {
       if (!card) {
-        next({ message: 'Нет карточки с указанным id' });
+        return res.status(404).send({ message: 'Нет карточки с указанным id' });
       }
       if (card.ownwer.toString() !== req.user._id) {
         next({ message: 'Запрещено удалять чужие карточки' });
@@ -33,8 +33,48 @@ module.exports.cardDelete = (req, res, next) => {
 
       return Card.deleteOne(card)
         .then(() => {
-          res.send({ message: 'Карточка успешно удалена' });
+          res.status(200).send({ message: 'Карточка успешно удалена' });
         });
     })
     .catch(next);
+};
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: 'Нет карточки с указанным id' });
+      }
+      res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Ошибка в введенных данных' });
+      }
+      res.status(500).send({ message: `Произошла ошибка в работе сервера ${err}` });
+    });
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: 'Нет карточки с указанным id' });
+      }
+      res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Ошибка в введенных данных' });
+      }
+      res.status(500).send({ message: `Произошла ошибка в работе сервера ${err}` });
+    });
 };
--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,12 @@
 const cardRoutes = require('express').Router();
-const { getCards, createCard, cardDelete } = require('../controllers/cards');
+const {
+  getCards, createCard, cardDelete, likeCard, dislikeCard,
+} = require('../controllers/cards');
 
 cardRoutes.get('/cards', getCards);
 cardRoutes.post('/cards', createCard);
 cardRoutes.delete('/cards/:id', cardDelete);
+cardRoutes.put('/cards/:cardId/likes', likeCard);
+cardRoutes.delete('/cards/:cardId/likes', dislikeCard);
 
 module.exports = cardRoutes;
